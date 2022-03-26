7dcaf488c941c64400cc942a0989ed80b2f102e7
deltah404 <oloskossi@gmail.com>
2022-03-26 22:20:54 +0000

    Bot frame

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import discord
-from time import sleep
 
-while True:
-    print("Hello World!")
-    sleep(0.11)
+token = os.environ['TOKEN']
+client = discord.Client()
+
+client.run(token)
