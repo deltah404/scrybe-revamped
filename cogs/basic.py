e39f01f136a6c6dc471636d6b00c2d6ce91536f1
deltah404 <oloskossi@gmail.com>
2022-03-27 16:14:11 +0100

    Command fix

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,6 +1,7 @@
 import discord
 from fetch import get_config
 from discord.ext import commands
+from discord.commands import slash_command
 
 guilds = get_config("guilds")
 
@@ -8,7 +9,7 @@ class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @bot.slash_command(guild_ids = guilds)
+    @slash_command(guild_ids = guilds)
     async def ping(self, ctx: commands.Context):
         await self.ctx.respond('Pong')
 
