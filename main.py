4728c697bc2603a06f66b6896e9159230b654cbb
deltah404 <oloskossi@gmail.com>
2022-03-27 13:48:31 +0100

    repair

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import discord
+
+token = os.environ['TOKEN']
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Booted as: {client.user}')
+
+client.run(token)
