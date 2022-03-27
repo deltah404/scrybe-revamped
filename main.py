77a4368f18dd1b63d3101166b08f5b3c1a912d10
deltah404 <oloskossi@gmail.com>
2022-03-27 15:35:05 +0100

    Command test

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import discord
 from discord.ext import commands
 
 token = os.environ['TOKEN']
-client = commands.Bot()
+bot = discord.Bot()
 
 # load extensions
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename}')
+        bot.load_extension(f'cogs.{filename}')
 
 @client.event
 async def on_ready():
     print(f'Booted as: {client.user}') # state when booted
 
-client.run(token)
+bot.run(token)
